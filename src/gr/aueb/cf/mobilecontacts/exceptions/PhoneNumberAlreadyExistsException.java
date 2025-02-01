49f50e683857c68ea43439da3e09646e8268fc0b
Nick Papadakis <orfneos@yahoo.gr>
2025-02-01 02:35:40 +0200

    update

---
+++ src/gr/aueb/cf/mobilecontacts/exceptions/PhoneNumberAlreadyExistsException.java
@@ -0,0 +1,10 @@
+package gr.aueb.cf.mobilecontacts.exceptions;
+
+public class PhoneNumberAlreadyExistsException extends Exception{
+
+    public PhoneNumberAlreadyExistsException(String message) {
+        super(message);
+    }
+
+
+}
